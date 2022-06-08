a339102410a55659074e3093913a14471987022b
GavinHe322 <https://github.com/GavinHe322>
2022-06-08 10:44:55 +0800

    ci: 通过 bootstrap 脚本创建相同的src packge.json等

---
+++ packages/compiler-core/index.js
@@ -0,0 +1,6 @@
+'use strict'
+      if (process.env.NODE_ENV  === 'production') {
+        module.exports  = require('./dist/compiler-core.cjs.prod.js')
+      } else {
+        module.exports =  require('./dist/compiler-core.cjs.js')
+      }
